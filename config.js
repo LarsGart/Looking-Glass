95512d8ca104abdb02f4f2279207ac0ee2bd9c0c
djpardo <djpardo@buffalo.edu>
2022-12-04 12:53:36 -0500

    Added customization

--- config.js
+++ config.js
@@ -37,16 +37,7 @@ let config = {
     // false, default for all NON-armv6l devices
     // true, force serveronly mode, because you want to.. no UI on this device
 
-    modules: [{
-            module: 'MMM-MTA-NextBus',
-            position: "bottom_right", // This can be any of the regions.
-            config: {
-                apiKey: "2adda606-21b2-4383-a827-a30d59440dea",
-                busStopCode: "200884",
-            },
-           
-            classes: 'ivan'
-        },
+    modules: [
         {
             module: "MMM-GroveGestures",
             position: "top_right",
@@ -54,7 +45,7 @@ let config = {
                 autoStart: true, //When Mirror starts, recognition will start.
                 verbose: false, // If set as `true`, useful messages will be logged.
                 recognitionTimeout: 1000, //Gesture sequence will be ended after this time from last recognized gesture.
-                cancelGesture: "WAVE", //If set, You can cancel gesture sequence with this gesture.
+                //cancelGesture: "WAVE", //If set, You can cancel gesture sequence with this gesture.
                 visible: true, //Recognized gesture sequence will be displayed on position
 
                 idleTimer: 1000 * 60 * 30, // `0` for disable, After this time from last gesture, onIdle will be executed.
@@ -149,25 +140,84 @@ let config = {
             },
            
             classes: 'lars'
-        }
-       
-        ,
+        },
+        {
+            module: 'MMM-Face-Reco-DNN',
+            config: {
+              // Logout 15 seconds after user was not detected any more
+              // If they are detected within this period, the delay will start again
+              logoutDelay: 15000,
+              // How often the recognition starts in milliseconds
+              // With a Raspberry Pi 3+ it works well every 2 seconds
+              checkInterval: 2000,
+              // Module set used for when there is no face detected ie no one is in front of the camera
+              noFaceClass: 'noface',
+              // Module set used for when there is an unknown/unrecognised face detected
+              unknownClass: 'unknown',
+              // Module set used for when there is a known/recognised face detected
+              knownClass: 'known',
+              // Module set used for strangers and if no user is detected
+              defaultClass: 'default',
+              // Set of modules which should be shown for any user ie when there is any face detected
+              everyoneClass: 'everyone',
+              // Set of modules that are always shown - show if there is a face or no face detected
+              alwaysClass: 'always',
+              // XML to recognize with haarcascade
+              cascade: 'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
+              // Pre-encoded pickle with the faces
+              encodings: 'modules/MMM-Face-Reco-DNN/tools/encodings.pickle',
+              // Use Raspberry Pi camera or another type
+              // 1 = RasPi camera, 0 = other camera
+              usePiCamera: 1,
+              // If using another type of camera, you can choose
+              // i.e. 0 = /dev/video0 or 'http://link.to/live'
+              source: 0,
+              // Rotate camera
+              rotateCamera: 0,
+              // Method of facial recognition
+              // dnn = deep neural network, haar = haarcascade
+              method: 'dnn',
+              // Which face detection model to use
+              // "hog" is less accurate but faster on CPUs
+              // "cnn" is a more accurate deep-learning model which is GPU/CUDA accelerated
+              detectionMethod: 'hog',
+              // How long in milliseconds modules take to hide and show
+              animationSpeed: 0,
+              // Path to Python to run the face recognition
+              // null or '' means default path
+              pythonPath: null,
+              // Should a welcome message be shown using the MagicMirror alerts module?
+              welcomeMessage: true,
+              // Dictionary for person name mapping in welcome message
+              // Allows for displaying name with complex character sets in welcome message e.g. jerome => Jérôme, hideyuki => 英之
+              usernameDisplayMapping: null,
+              // Capture new pictures of recognized people, if unknown we save it in folder "unknown"
+              // So you can extend your dataset and retrain it afterwards for better recognitions
+              extendDataset: false,
+              // If extendDataset is true, you need to set the full path of the dataset
+              dataset: 'modules/MMM-Face-Reco-DNN/dataset/',
+              // How much distance between faces to consider it a match. Lower is more strict.
+              tolerance: 0.6,
+              // allow multiple concurrent user logins, 0=no, any other number is the maximum number of concurrent logins
+              multiUser: 0,
+            }
+        },
         {
             module: "alert",
            
-            classes: 'lars'
+            classes: 'everyone'
         },
         {
             module: "updatenotification",
             position: "top_bar",
            
-            classes: 'lars'
+            classes: 'everyone'
         },
         {
             module: "clock",
             position: "top_left",
            
-            classes: 'lars'
+            classes: 'everyone'
         },
         {
             module: "calendar",
@@ -180,57 +230,7 @@ let config = {
                 }]
             },
            
-            classes: 'lars'
-        },
-        {
-            module: "MMM-Jast",
-            position: "bottom_left",
-            config: {
-                currencyStyle: "code", // One of ["code", "symbol", "name"]
-                fadeSpeedInSeconds: 3.5,
-                lastUpdateFormat: "HH:mm",
-                maxChangeAge: 1 * 24 * 60 * 60 * 1000,
-                maxWidth: "100%",
-                numberDecimalsPercentages: 1,
-                numberDecimalsValues: 2,
-                scroll: "none", // One of ["none", "vertical", "horizontal"]
-                showColors: true,
-                showCurrency: true,
-                showChangePercent: true,
-                showChangeValue: false,
-                showChangeValueCurrency: false,
-                showHiddenStocks: false,
-                showLastUpdate: false,
-                showPortfolioValue: false,
-                showPortfolioGrowthPercent: false,
-                showPortfolioGrowth: false,
-                updateIntervalInSeconds: 300,
-                useGrouping: false,
-                virtualHorizontalMultiplier: 2,
-                stocks: [{
-                        name: "Apple",
-                        symbol: "AAPL",
-                        quantity: 10
-                    },
-                    {
-                        name: "Tesla",
-                        symbol: "TSLA",
-                        quantity: 15
-                    },
-                    {
-                        name: "Twitter",
-                        symbol: "TWTR",
-                        hidden: true
-                    },
-                    {
-                        name: "Netflix",
-                        symbol: "NFLX",
-                        quantity: 20
-                    }
-                ]
-            },
-           
-            classes: 'daniel'
+            classes: 'everyone'
         },
         {
             module: "weather",
@@ -243,7 +243,7 @@ let config = {
                 apiKey: "23ecee5a8c9ab24a8fb4d674493bf892"
             },
            
-            classes: 'lars'
+            classes: 'everyone'
         },
         {
             module: "weather",
@@ -257,8 +257,19 @@ let config = {
                 apiKey: "23ecee5a8c9ab24a8fb4d674493bf892"
             },
            
-            classes: 'lars'
+            classes: 'everyone'
         },
+        {
+			module: 'facts',
+			position: 'bottom_bar',
+			config: {
+					updateInterval: 10,	
+					fadeSpeed: 4,			
+					category: 'random',	
+			},
+
+            classes: 'unknown'
+		},
         {
             module: "newsfeed",
             position: "bottom_bar",
@@ -273,69 +284,63 @@ let config = {
                 broadcastNewsUpdates: true
             },
            
-            classes: 'always'
+            classes: 'daniel'
         },
         {
-    module: 'MMM-Face-Reco-DNN',
-    config: {
-      // Logout 15 seconds after user was not detected any more
-      // If they are detected within this period, the delay will start again
-      logoutDelay: 15000,
-      // How often the recognition starts in milliseconds
-      // With a Raspberry Pi 3+ it works well every 2 seconds
-      checkInterval: 2000,
-      // Module set used for when there is no face detected ie no one is in front of the camera
-      noFaceClass: 'noface',
-      // Module set used for when there is an unknown/unrecognised face detected
-      unknownClass: 'unknown',
-      // Module set used for when there is a known/recognised face detected
-      knownClass: 'known',
-      // Module set used for strangers and if no user is detected
-      defaultClass: 'default',
-      // Set of modules which should be shown for any user ie when there is any face detected
-      everyoneClass: 'everyone',
-      // Set of modules that are always shown - show if there is a face or no face detected
-      alwaysClass: 'always',
-      // XML to recognize with haarcascade
-      cascade: 'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
-      // Pre-encoded pickle with the faces
-      encodings: 'modules/MMM-Face-Reco-DNN/tools/encodings.pickle',
-      // Use Raspberry Pi camera or another type
-      // 1 = RasPi camera, 0 = other camera
-      usePiCamera: 1,
-      // If using another type of camera, you can choose
-      // i.e. 0 = /dev/video0 or 'http://link.to/live'
-      source: 0,
-      // Rotate camera
-      rotateCamera: 0,
-      // Method of facial recognition
-      // dnn = deep neural network, haar = haarcascade
-      method: 'dnn',
-      // Which face detection model to use
-      // "hog" is less accurate but faster on CPUs
-      // "cnn" is a more accurate deep-learning model which is GPU/CUDA accelerated
-      detectionMethod: 'hog',
-      // How long in milliseconds modules take to hide and show
-      animationSpeed: 0,
-      // Path to Python to run the face recognition
-      // null or '' means default path
-      pythonPath: null,
-      // Should a welcome message be shown using the MagicMirror alerts module?
-      welcomeMessage: true,
-      // Dictionary for person name mapping in welcome message
-      // Allows for displaying name with complex character sets in welcome message e.g. jerome => Jérôme, hideyuki => 英之
-      usernameDisplayMapping: null,
-      // Capture new pictures of recognized people, if unknown we save it in folder "unknown"
-      // So you can extend your dataset and retrain it afterwards for better recognitions
-      extendDataset: false,
-      // If extendDataset is true, you need to set the full path of the dataset
-      dataset: 'modules/MMM-Face-Reco-DNN/dataset/',
-      // How much distance between faces to consider it a match. Lower is more strict.
-      tolerance: 0.6,
-      // allow multiple concurrent user logins, 0=no, any other number is the maximum number of concurrent logins
-      multiUser: 0,
-    }
-}
+            module: 'MMM-jokes',
+            position: 'lower_third',	// This can be any of the regions.
+                            // Best results in one of the middle regions like: lower_third
+            config: {
+                api: 'icndb' //required
+            },
+
+            classes: 'ivan'
+        },
+        {
+            module: 'MMM-MTA-NextBus',
+            position: "bottom_right", // This can be any of the regions.
+            config: {
+                apiKey: "2adda606-21b2-4383-a827-a30d59440dea",
+                busStopCode: "200884",
+            },
+           
+            classes: 'ivan'
+        },
+        {
+            module: 'MMM-NFL',
+            position: 'bottom_right',
+            config: {
+                colored: true,
+                reloadInterval: 1800000,
+                tableSize: 'small'
+            },
+
+            classes: 'daniel'
+        },
+        {
+            module: 'MMM-SystemStats',
+            position: 'bottom_left', // This can be any of the regions.
+            // classes: 'small dimmed', // Add your own styling. OPTIONAL.
+            // header: 'System Stats', // Set the header text OPTIONAL
+            config: {
+                updateInterval: 10000, // every 10 seconds
+                align: 'right', // align labels
+                header: 'System Stats', // This is optional
+                units: 'metric', // default, metric, imperial
+                view: 'textAndIcon',
+            },
+
+            classes: 'known'
+        },
+        {
+            module: 'MMM-NetworkConnection',
+            position: 'bottom_left',
+            config: {
+            },
+
+            classes: 'known'
+        }
+        
     ]
 };
 
